25436347bfd5f6c6d78c67394d60fd2d5f72880f
Gulya Bulatova <bulatova-guziul@mail.ru>
2020-10-14 18:53:26 +0300

    add task 3

--- src/Controller.php
+++ src/Controller.php
@@ -1,9 +1,18 @@
-<?php 
-	namespace GulyaBulatova\guessnumber\Controller;
-	use function GulyaBulatova\guessnumber\View\showGame;
-	
-	function startGame() {
-		echo "Game started".PHP_EOL;
-		showGame();
-	}
+<?php
+
+namespace GulyaBulatova\guessnumber\Controller;
+
+use function GulyaBulatova\guessnumber\View\showGame;
+use function GulyaBulatova\guessnumber\View\setting;
+use function GulyaBulatova\guessnumber\View\greeting;
+use function GulyaBulatova\guessnumber\View\endGame;
+use function GulyaBulatova\guessnumber\View\replayGame;
+
+function startGame()
+{
+    setting();
+    greeting();
+    showGame();
+}
+
 ?>
